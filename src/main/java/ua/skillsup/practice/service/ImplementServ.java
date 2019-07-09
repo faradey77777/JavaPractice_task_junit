7b5b320ecbe9045ac7f0b13c645f41cd7ffabb5a
faradey77777 <Faradey11061992>
2019-07-09 11:19:20 +0300

    create first test.

--- src/main/java/ua/skillsup/practice/service/ImplementServ.java
+++ src/main/java/ua/skillsup/practice/service/ImplementServ.java
@@ -12,25 +12,19 @@ import java.time.Instant;
 
 
 public class ImplementServ implements ExampleService {
-
+    private boolean storeExam;
+    private ExampleDao exampleDao;
+    private ExampleEntity exampleEntity;
     private long id = 0;
 
     public ImplementServ(ExampleDao exampleDao) {
     }
-
-
-    public boolean isStoreExam() {
-        return storeExam;
-    }
-
     public void setStoreExam(boolean storeExam) {
         this.storeExam = storeExam;
     }
-
-    private boolean storeExam;
-    private ExampleDao exampleDao;
-    private ExampleEntity exampleEntity;
-
+    public boolean isStoreExam() {
+        return storeExam;
+    }
 
     @Override
     public void addNewItem(String title, BigDecimal price) throws Exception {
@@ -43,7 +37,6 @@ public class ImplementServ implements ExampleService {
         ExampleEntity entity = new ExampleEntity();
         entity.setTitle(title);
         entity.setPrice(price);
-
         storeExam = exampleDao.store(entity);
 
     }
@@ -52,7 +45,6 @@ public class ImplementServ implements ExampleService {
     @Override
     public Map<LocalDate, BigDecimal> getStatistic() {
         exampleDao.findAll();
-
         return null;
     }
 }
